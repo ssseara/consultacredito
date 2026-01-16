1820545d75c30b36ab830a513c75084ab0330188
luiz <luiz@fedora>
2026-01-16 01:38:04 +0000

    Commit inicial do projeto

---
+++ src/main/java/br/com/gestionna/consultacredito/ConsultacreditoApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestionna.consultacredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultacreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultacreditoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gestionna/consultacredito/ConsultacreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestionna.consultacredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultacreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
